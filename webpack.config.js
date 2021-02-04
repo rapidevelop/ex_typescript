e05693bfb170240d75a0b2bad61a197263e7ae47
rapidevelop <i.vafadarnezhad@gmail.com>
2021-02-04 19:41:31 +0330

    chore: install webpack
    install webpack and configure it to compile typescript and ecmascript 2015+ using babel-loader and
    ts-loader

---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+const path = require('path');
+const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.ts',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+  },
+  devtool: 'eval',
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js', '.json'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        exclude: /(node_modules)/,
+        use: [
+          'babel-loader',
+          {
+            loader: 'ts-loader',
+            options: {
+              transpileOnly: true,
+            },
+          },
+        ],
+      },
+      {
+        test: /\.jsx?$/,
+        exclude: /(node_modules)/,
+        loader: 'babel-loader',
+      },
+    ],
+  },
+  plugins: [new ForkTsCheckerWebpackPlugin()],
+};
